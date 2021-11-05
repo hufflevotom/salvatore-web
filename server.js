58c4a5ef3d02ddcc5654b335a45538953f122e4b
hufflevotom <ten.crypto@outlook.com>
2021-11-04 21:38:34 -0500

    deploy heroku

--- server.js
+++ server.js
@@ -1,19 +1,16 @@
-function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-}
-
+//Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-app.use(requireHTTPS);
-app.use(express.static('./dist/salvatore-frontend'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/salvatore-frontend'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/salvatore-frontend/'}),
-);
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/salvatore-frontend/index.html'));
+});
 
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
