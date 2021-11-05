ff28eb9e19004320270ebcb182dc26327c42f925
hufflevotom <ten.crypto@outlook.com>
2021-11-05 00:58:00 -0500

    Reverse commits

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  URL_API: 'https://salvatore-backend.herokuapp.com/v1',
+  serverSocket: 'https://salvatore-backend.herokuapp.com:5000',
+  // URL_API: 'http://localhost:4000/v1',
+  // serverSocket: 'http://localhost:5000'
 };
