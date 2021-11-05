bd1ae1931a97616d35297c27d77a95071ea3c205
hufflevotom <ten.crypto@outlook.com>
2021-11-04 21:20:52 -0500

    env production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  URL_API: 'https://salvatore-backend.herokuapp.com/v1',
+  serverSocket: 'https://salvatore-backend.herokuapp.com:5000',
 };
