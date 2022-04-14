d853256bcd17b8919bee62dc15c64a2023933410
Huffle Votom <ten.crypto@outlook.com>
2022-04-14 00:54:05 -0500

    CloudFlare recovery

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Salvatore';
+  constructor(private titleService: Title, private metaService: Meta) {}
+
+  ngOnInit() {
+    this.titleService.setTitle(this.title);
+    this.metaService.addTags([
+      { name: 'cf-2fa-verify', content: 'fxME8BRBrLBx8QyVeaRgDYUH31Lcn7' },
+    ]);
+  }
 }
